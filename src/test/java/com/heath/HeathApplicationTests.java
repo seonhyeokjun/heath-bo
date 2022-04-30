2fb917ef39c8d62dc530af3a8f4669857d923a45
seonhyeokjun <tjsrmatn@naver.com>
2022-04-30 23:52:19 +0900

    Initial commit

---
+++ src/test/java/com/heath/HeathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
